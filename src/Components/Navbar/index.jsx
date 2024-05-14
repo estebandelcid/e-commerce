53fff776ec355f531e6c7f860bd8a4a90bcbdf1a
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-14 16:38:56 -0600

    [fix] Nav css top 0

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export const Navbar = () => {
     const activeStyle = 'underline underline-offset-4'
     return (
-        <nav className=" w-full flex justify-between items-center fixed z-10 py-5 px-8 text-sm font-light">
+        <nav className=" w-full flex justify-between items-center fixed top-0 z-10 py-5 px-8 text-sm font-light">
             <ul className=" flex items-center gap-4">
                 <li className=" font-semibold text-lg">
                     <NavLink to='/' >
