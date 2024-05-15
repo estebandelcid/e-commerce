8ff5cbe9f1b52a191d7d1fa64590f00174a6913f
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-15 04:28:40 -0600

    [add] Función para manejar el evento de click en la tarjeta donde nos abre el aside de detail y manda la info al estado de productToShow. stopPropagation para evitar que al momento de dar a agregar al carrito, se habra el product detail.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,6 +4,10 @@ import { ShoppingCartContext } from "../../Context";
 
 export const Card = (props) => {
     const context = useContext(ShoppingCartContext);
+    const showProduct = (productDetail) => {
+      context.openProductDetail()
+      context.setProductToShow(productDetail)
+    }
     const handleImageError = (event) => {
       event.target.src =
         "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png";
@@ -11,7 +15,7 @@ export const Card = (props) => {
     };
     return (
       <div className=" w-56 h-60 bg-white cursor-pointer rounded-lg border border-black/20"
-      onClick={() => context.openProductDetail()}
+      onClick={() => showProduct(props.data)}
       >
         <figure className=" w-full h-4/5 relative mb-2 ">
           <span className=" absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
@@ -24,7 +28,10 @@ export const Card = (props) => {
             onError={handleImageError}
           />
           <button className=" w-6 h-6 absolute top-0 right-0 bg-white rounded-full flex justify-center items-center m-2 p-1"
-          onClick={() => context.setCount(context.count + 1)}
+          onClick={(event) => {
+            event.stopPropagation()
+            context.setCount(context.count + 1)
+          }}
           >
             <PlusIcon className="w-6 h-6 text-black" />
           </button>
