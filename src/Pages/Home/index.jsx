f2750823a393414d97224d1edf63f000306dd738
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-15 00:46:33 -0600

    [add] Se usó useState para guardar la data que conseguimos por medio de hacer un fetch a la API haciendo uso de useEffect y dentro, una función asíncrona para después pintar los productos haciendo uso de Card.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
 import { Layout } from "../../Components/Layout";
+import { Card } from "../../Components/Card";
 
 export const Home = () => {
-  return <Layout>Home</Layout>;
+  const [items, setItems] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/products"
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error("Hubo un error: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Layout>
+      Home
+      {items?.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+    </Layout>
+  );
 };
