e6d072baacc4ac7b7b4efbc1ab90af28c14e405d
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-15 03:40:20 -0600

    [add] Condicional para mostrar o no el aside por medio del estado que se recibe desde el contexto y se le pasa la función para cerrar el aside al botón close.

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,16 @@
 import { XMarkIcon } from "@heroicons/react/24/solid"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 
 export const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext);
     return(
-        <aside className=" w-[360px] h-[calc(100vh-80px)] flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-80px)] flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className=" flex justify-between items-center p-6 ">
                 <h2 className=" font-medium text-xl">Detail</h2>
-                <div>
+                <button onClick={() => context.closeProductDetail()}>
                     <XMarkIcon className="w-6 h-6 text-black"/>
-                </div>
+                </button>
             </div>
         </aside>
     )
