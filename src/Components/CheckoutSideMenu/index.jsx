101908020cb1b5d15e095ba40aed7a8b43d74700
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-20 15:21:26 -0600

    [add] Base del componente checkout.

---
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -0,0 +1,17 @@
+import { XMarkIcon } from "@heroicons/react/24/solid"
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
+
+export const CheckoutSideMenu = () => {
+    const context = useContext(ShoppingCartContext);
+    return(
+        <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-68px)] flex-col fixed top-[68px] right-0 border border-black/20 rounded-lg bg-white`}>
+            <div className=" flex justify-between items-center p-6 ">
+                <h2 className=" font-medium text-xl">My Order</h2>
+                <button onClick={() => context.closeCheckoutSideMenu()}>
+                    <XMarkIcon className="w-6 h-6 text-black"/>
+                </button>
+            </div>
+        </aside>
+    )
+};
