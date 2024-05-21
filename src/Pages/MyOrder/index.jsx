01aabc5f3c072b0192fb3bea18bfa7bf5d7f7911
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-21 14:44:05 -0600

    [add] Flecha que lleva a las ordenes.

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,27 +1,33 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
 import { OrderCard } from "../../Components/OrderCard"
-
 import { Layout } from "../../Components/Layout";
 
 export const MyOrder = () => {
   const context = useContext(ShoppingCartContext);
 
     return (
-    <Layout>
-      My Order
-      <div className=" w-80 flex flex-col">
-        {context.order?.slice(-1)[0].products.map((product) => (
-          <OrderCard
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            imageUrl={product.images?.[0]}
-            price={product.price}
-          />
-        ))}
+      <Layout>
+        <div className="w-80 relative flex justify-center items-center mb-6">
+          <Link to={"/my-orders"} className="absolute left-0">
+            <ChevronLeftIcon className="w-6 h-6 text-black" />
+          </Link>
+          <h1>My Orders</h1>
         </div>
-    </Layout>
-    )
+        <div className=" w-80 flex flex-col">
+          {context.order?.slice(-1)[0].products.map((product) => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.images?.[0]}
+              price={product.price}
+            />
+          ))}
+        </div>
+      </Layout>
+    );
   };
   
