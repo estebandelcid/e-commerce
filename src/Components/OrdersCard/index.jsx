188058959fe841d413f9625004acc5edcd43628d
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-21 16:19:44 -0600

    [add] Usando Date.

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,11 +1,11 @@
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export const OrdersCard = (props) => {
-    const { totalPrice, totalProducts } = props;
+    const { totalPrice, totalProducts, date } = props;
   return (
     <div className="flex justify-between items-center mb-3 border border-black">
      <p>
-        <span>01.02.24</span>
+        <span>{date}</span>
         <span>{totalProducts}</span>
         <span>{totalPrice}</span>
      </p>
