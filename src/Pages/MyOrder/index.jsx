9b8b3d159e161f349dce0423178bb84fa8e5185d
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-21 01:50:32 -0600

    [add] Componente que pinta la ultima orden creada.

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,6 +1,27 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import { OrderCard } from "../../Components/OrderCard"
+
 import { Layout } from "../../Components/Layout";
 
 export const MyOrder = () => {
-    return <Layout>My Order</Layout>;
+  const context = useContext(ShoppingCartContext);
+
+    return (
+    <Layout>
+      My Order
+      <div className=" w-80 flex flex-col">
+        {context.order?.slice(-1)[0].products.map((product) => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.images?.[0]}
+            price={product.price}
+          />
+        ))}
+        </div>
+    </Layout>
+    )
   };
   
