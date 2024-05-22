fc1116cbd6fae1e1613b7083ac3fb6e178b5533c
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-22 02:01:23 -0600

    [add] Icono.

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,15 @@
+import { WrenchScrewdriverIcon } from "@heroicons/react/24/solid";
 import { Layout } from "../../Components/Layout";
 
 export const SignIn = () => {
-    return <Layout>Sign In</Layout>;
-  };
+  return (
+    <Layout>
+      Sign In
+      <p>Sorry, construction in progress...</p>
+      <span>
+        <WrenchScrewdriverIcon className="w-10 h-10 text-black"/>
+      </span>
+    </Layout>
+  );
+};
   
