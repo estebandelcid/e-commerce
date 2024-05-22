656b3da8389ce06a755354398281ba68e881c4de
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-21 22:16:51 -0600

    [fix] Pasando category.

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -42,7 +42,7 @@ export const Card = (props) => {
       >
         <figure className=" w-full h-4/5 relative mb-2 ">
           <span className=" absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-            {props.data.category.name}
+            {props.data.category}
           </span>
           <img
             className=" w-full h-full object-cover rounded-lg"
