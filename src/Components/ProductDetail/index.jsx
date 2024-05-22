e22c2ad6232485d89682c59d1f1dce4bdab1e7ad
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-22 01:51:04 -0600

    [fix] Ajuste de css en el top del aside.

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -6,7 +6,7 @@ import { handleImageError } from "../../Utils/imageUtils";
 export const ProductDetail = () => {
     const context = useContext(ShoppingCartContext);
     return(
-        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-80px)] flex-col fixed right-0 border border-black/20 rounded-lg bg-white`}>
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} w-[360px] h-[calc(100vh-68px)] flex-col fixed top-[68px] right-0 border border-black/20 rounded-lg bg-white`}>
             <div className=" flex justify-between items-center p-6 ">
                 <h2 className=" font-medium text-xl">Detail</h2>
                 <button onClick={() => context.closeProductDetail()}>
