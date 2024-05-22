97208ec6084e87dcca2a8e0cf35c3d1f42c87abd
Esteban Del Cid <estebandelcid.07@gmail.com>
2024-05-21 22:29:15 -0600

    [add] Base de input para realizar filtrados.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,11 +6,19 @@ import { ShoppingCartContext } from "../../Context";
 
 export const Home = () => {
   const context = useContext(ShoppingCartContext);
-  8
+  
 
   return (
     <Layout>
-      Home
+      <div className="w-80 flex justify-center items-center mb-4">
+        <h1 className=" font-medium text-xl">Exclusive Products</h1>
+      </div>
+      <input 
+      className=" rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+      type="text" 
+      placeholder="Search product"
+      onChange={(event) => context.setSearchByTitle(event.target.value)}
+      />
       <div className=" w-full max-w-screen-lg grid grid-cols-4 gap-4">
       {context.items?.products.map((item) => (
         <Card key={item.id} data={item} />
